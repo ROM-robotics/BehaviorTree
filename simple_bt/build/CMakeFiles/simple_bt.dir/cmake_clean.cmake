b28642628d78b5cdd94450a9ffe1dff19c1578af
pyaesoanaung <romrobotics@gmail.com>
2023-05-08 09:59:31 +0630

    simple_bt

---
+++ simple_bt/build/CMakeFiles/simple_bt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_bt.dir/simple_bt.cpp.o"
+  "CMakeFiles/simple_bt.dir/simple_bt.cpp.o.d"
+  "simple_bt"
+  "simple_bt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_bt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
